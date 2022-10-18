305251d7483753f618bdf52e69fbfe9e9705ffc0
FOA1 <fataia500@gmail.com>
2022-10-18 13:02:52 +0000

    Printf

--- test/main.c
+++ test/main.c
@@ -23,15 +23,17 @@ int main(void)
   int len;
   
   int len2;
-
+  
   unsigned int ui;
   
   void *addr;
   
+
+  
   len = _printf("Let's try to printf a simple sentence.\n");
   
   len2 = printf("Let's try to printf a simple sentence.\n");
-
+  
   ui = (unsigned int)INT_MAX + 1024;
   
   addr = (void *)0x7ffe637541f0;
@@ -78,9 +80,8 @@ int main(void)
   
   _printf("Unknown:[%r]\n");
   
-  printf("Unknown:[%r]\n");
+  /* printf("Unknown:[%r]\n"); */
   
   return (0);
   
-}
 }
--- test/main.h
+++ test/main.h
@@ -5,22 +5,10 @@
 #include <stdarg.h>
 #include <stdint.h>
 
-/**
- * struct sp - struct
- *
- * @sp: the specifier
- * @f: the function associated
- */
-typedef struct sp
-{
-	char *sp;
-
-	int (*f)(va_list);
-} struc;
-
 int _putchar(char c);
 void unsigned_number_to_string(uint64_t number, int base, char *buffer);
-void number_to_string(int64_t number, int base, char *buffer);
+void number_to_string(uint64_t number, int base, char *buffer);
+int _vprintf(const char *format, va_list args);
 int _printf(const char *format, ...);
 void _puts(char *str);
 void print_number(int n);
